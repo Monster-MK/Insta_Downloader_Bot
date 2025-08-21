87e8082900c39efeb3af7847ef73e09584682c24
MƠƝƧƬЄƦ MƘ 🔥 <hardikpandya4849@gmail.com>
2025-08-21 22:11:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,19 +5,19 @@ from dotenv import load_dotenv
 load_dotenv("config.env")
 
 # Telegram Bot Settings
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+BOT_TOKEN = os.getenv("8369660708:AAHeMEjpIj8r9J9EXp4n4E5z0FQIwdtW-zs")
+API_ID = int(os.getenv("20517170"))
+API_HASH = os.getenv("f09e5c91dd864f01063ff63827832137")
+OWNER_ID = int(os.getenv("8060684565"))
 
 PICS = (environ.get('PICS', 'https://files.catbox.moe/7ugjlq.jpg')).split()  # Sample pic
 
 # Channels
-INSTA_CHANNEL = int(os.getenv("INSTA_CHANNEL"))      # For storing Instagram media
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))          # For errors, status updates
+INSTA_CHANNEL = int(os.getenv("-1002527891441"))      # For storing Instagram media
+LOG_CHANNEL = int(os.getenv("-1002845044839"))          # For errors, status updates
 
 # Database
-MONGO_DB_URI = os.getenv("MONGO_DB_URI")             # MongoDB connection string
+MONGO_DB_URI = os.getenv("mongodb+srv://mdhanush8377:9cQl4L7iYAcXxggI@cluster0.wvfwtg9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")             # MongoDB connection string
 DB_NAME = os.getenv("DB_NAME", "InstaDownloaderDB")  # Default DB name
 
 # Captions
