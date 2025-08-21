e91b45cdbeaf6525c3ac0f1ad563ff08457aa361
MƠƝƧƬЄƦ MƘ 🔥 <hardikpandya4849@gmail.com>
2025-08-21 22:31:56 +0530

    Update and rename config.env to info.py

--- info.py
+++ info.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv("config.env")
 
 # Telegram Bot Settings
-BOT_TOKEN = os.getenv("8369660708:AAHeMEjpIj8r9J9EXp4n4E5z0FQIwdtW-zs")
-API_ID = int(os.getenv("20517170")
-API_HASH = os.getenv("f09e5c91dd864f01063ff63827832137")
-OWNER_ID = int(os.getenv("8060684565")
+BOT_TOKEN = 8369660708:AAHeMEjpIj8r9J9EXp4n4E5z0FQIwdtW-zs
+API_ID = 20517170
+API_HASH = f09e5c91dd864f01063ff63827832137
+OWNER_ID = 8060684565
 
 PICS = (environ.get('PICS', 'https://files.catbox.moe/7ugjlq.jpg')).split()  # Sample pic
 
